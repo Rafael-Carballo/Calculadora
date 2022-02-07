ae97bb8d157fe924d7c9e733388a88697ead8c3e
Rafael-Carballo <74878734+Rafael-Carballo@users.noreply.github.com>
2022-02-07 01:50:27 -0800

    funcionalidad de botones
    funcionalidad de los botones numericos

--- app/src/main/java/carballo/rafael/calculadora/MainActivity.kt
+++ app/src/main/java/carballo/rafael/calculadora/MainActivity.kt
@@ -2,6 +2,7 @@ package carballo.rafael.calculadora
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
@@ -10,7 +11,69 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val txArriba: TextView= findViewById(R.id.arriba)
+        val txAbajo: TextView= findViewById(R.id.abajo)
+        val b1:Button=findViewById(R.id.b1)
+        val b2:Button=findViewById(R.id.b2)
+        val b3:Button=findViewById(R.id.b3)
+        val b4:Button=findViewById(R.id.b4)
+        val b5:Button=findViewById(R.id.b5)
+        val b6:Button=findViewById(R.id.b6)
+        val b7:Button=findViewById(R.id.b7)
+        val b8:Button=findViewById(R.id.b8)
+        val b9:Button=findViewById(R.id.b9)
+        val b0:Button=findViewById(R.id.b0)
+        val badd:Button=findViewById(R.id.badd)
+        val bsus:Button=findViewById(R.id.bsus)
+        val bdiv:Button=findViewById(R.id.bdiv)
+        val btimes:Button=findViewById(R.id.btimes)
+        val bc:Button=findViewById(R.id.bc)
+        val bequal:Button=findViewById(R.id.bequals)
+
+        fun limpia(){
+            txArriba.text=""
+            txAbajo.text="0"
+        }
+        fun cadena(numero:Int){
+            var aux:String=txAbajo.text.toString()
+            if(aux.equals("0")){
+                aux=""
+            }
+            aux=aux+""+numero
+            txAbajo.text = aux
+        }
         
+        b1.setOnClickListener{
+            cadena(1)
+        }
+        b2.setOnClickListener{
+            cadena(2)
+        }
+        b3.setOnClickListener{
+            cadena(3)
+        }
+        b4.setOnClickListener{
+            cadena(4)
+        }
+        b5.setOnClickListener{
+            cadena(5)
+        }
+        b6.setOnClickListener{
+            cadena(6)
+        }
+        b7.setOnClickListener{
+            cadena(7)
+        }
+        b8.setOnClickListener{
+            cadena(8)
+        }
+        b9.setOnClickListener{
+            cadena(9)
+        }
+        b0.setOnClickListener{
+            cadena(0)
+        }
+
+
 
 
     }
