b139a76270e15c9982cd5f00715c7af9b3f146ec
Rafael-Carballo <74878734+Rafael-Carballo@users.noreply.github.com>
2022-02-07 00:20:50 -0800

    grid de botones
    interfaz grafica lista

--- app/src/main/java/carballo/rafael/calculadora/MainActivity.kt
+++ app/src/main/java/carballo/rafael/calculadora/MainActivity.kt
@@ -2,10 +2,16 @@ package carballo.rafael.calculadora
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val txArriba: TextView= findViewById(R.id.arriba)
+        
+
+
     }
 }
